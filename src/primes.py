1014f14550f4c24106b4b2daac761ca8c1e6cf4b
candelarojas1 <candelarojas2001@gmail.com>
2025-03-26 21:11:55 -0300

    Agregados comentarios al código en primes.py

--- src/primes.py
+++ src/primes.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-# Python program to display all the prime numbers within an interval
+# Programa en Python para mostrar todos los números primos dentro de un intervalo
 
-lower = 1
-upper = 500
+# Definimos los límites del intervalo
+lower = 1  # Límite inferior
+upper = 500  # Límite superior
 
-print("Prime numbers between", lower, "and", upper, "are:")
+# Imprimimos un mensaje indicando el rango en el que buscaremos números primos
+print("Números primos entre", lower, "y", upper, "son:")
 
+# Recorremos todos los números dentro del intervalo especificado
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+    # Los números primos son mayores que 1
+    if num > 1:
+        # Verificamos si el número es divisible por algún otro número menor que él mismo
+        for i in range(2, num):
+            # Si es divisible por otro número distinto de 1 y de sí mismo, no es primo
+            if (num % i) == 0:
+                break  # Salimos del bucle si encontramos un divisor
+        else:
+            # Si no encontramos divisores, el número es primo y lo imprimimos
+            print(num)
